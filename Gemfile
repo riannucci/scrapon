cc16895646410f5083fcb7d25474e4ac57145cb1
Robert A. Iannucci Jr <robbie@rail.com>
2012-01-27 04:42:54 -0800

    add Gemfile

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'mechanize'
+gem 'progressbar'
+gem 'logger'
+gem 'andand'
+gem 'chronic'
+gem 'progressbar'
--- main.rb
+++ main.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby-local-exec
-require './groupon_spider'
+require 'rubygems'
+require 'bundler/setup'
+
 require 'yaml'
+require './groupon_spider'
 
 p GrouponSpider.new(YAML.load_file('options.yml')).data.map { |i| i[:name] }
